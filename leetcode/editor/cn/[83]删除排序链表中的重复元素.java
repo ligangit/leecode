4f01eb55fb53d2e9e456f7bd39de4efece2c76a4
ligangit <913489864@qq.com>
2023-02-01 22:35:41 +0800

    83-删除排序链表中的重复元素

---
+++ leetcode/editor/cn/[83]删除排序链表中的重复元素.java
@@ -0,0 +1,79 @@
+//给定一个已排序的链表的头
+// head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,1,2]
+//输出：[1,2]
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：head = [1,1,2,3,3]
+//输出：[1,2,3]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目在范围 [0, 300] 内 
+// -100 <= Node.val <= 100 
+// 题目数据保证链表已经按升序 排列 
+// 
+//
+// Related Topics 链表 👍 932 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+//    public ListNode deleteDuplicates(ListNode head) {
+//        if (head == null) {
+//            return head;
+//        }
+//
+//        ListNode temp = head;
+//        while (temp.next != null) {
+//            if (temp.val == temp.next.val) {
+//                temp.next = temp.next.next;
+//            } else {
+//                temp = temp.next;
+//            }
+//        }
+//        return head;
+//    }
+
+    /**
+     * 递归处理
+     * @param head
+     * @return
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        head.next = deleteDuplicates(head.next);
+        if (head.val == head.next.val) {
+            return head.next;
+        } else {
+            return head;
+        }
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
