fbee7503936ed725a681bbb842e10b6c217912b1
ligang <ligang@szltech.com>
2025-01-24 10:58:09 +0800

    67-二进制求和-字符串遍历

--- leetcode/editor/cn/[67]二进制求和.java
+++ leetcode/editor/cn/[67]二进制求和.java
@@ -30,43 +30,64 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String addBinary(String a, String b) {
-        char[] charArrayLong = a.toCharArray();
-        char[] charArrayShort = b.toCharArray();
-        char[] resultArray;
-        if (a.length() >= b.length()) {
-            resultArray = new char[a.length()];
-            charArrayLong = a.toCharArray();
-            charArrayShort = b.toCharArray();
 
-        }else {
-            resultArray = new char[b.length()];
-            charArrayLong = b.toCharArray();
-            charArrayShort = a.toCharArray();
-        }
-        String temp = "0";
-        for (int i = charArrayLong.length - 1; i >= 0; i--) {
-            // 计算对应短字符串位置的下标
-            int j = charArrayShort.length + i - charArrayLong.length;
-            int he = Integer.valueOf(temp) + Integer.valueOf(String.valueOf(charArrayLong[i]));
+        // 方式一：遍历字符串转char计算
+//        char[] charArrayLong = a.toCharArray();
+//        char[] charArrayShort = b.toCharArray();
+//        char[] resultArray;
+//        if (a.length() >= b.length()) {
+//            resultArray = new char[a.length()];
+//            charArrayLong = a.toCharArray();
+//            charArrayShort = b.toCharArray();
+//
+//        }else {
+//            resultArray = new char[b.length()];
+//            charArrayLong = b.toCharArray();
+//            charArrayShort = a.toCharArray();
+//        }
+//        String temp = "0";
+//        for (int i = charArrayLong.length - 1; i >= 0; i--) {
+//            // 计算对应短字符串位置的下标
+//            int j = charArrayShort.length + i - charArrayLong.length;
+//            int he = Integer.valueOf(temp) + Integer.valueOf(String.valueOf(charArrayLong[i]));
+//            if (j >= 0) {
+//                he = he + Integer.valueOf(String.valueOf(charArrayShort[j]));
+//            }
+//            if (he == 2) {
+//                temp = "1";
+//                he = 0;
+//            } else if (he == 3) {
+//                temp = "1";
+//                he = 1;
+//            } else {
+//                temp = "0";
+//            }
+//            resultArray[i] = (char) (he + '0');
+//        }
+//        String s = new String(resultArray);
+//        if (temp.equals("1")) {
+//            s = temp + s;
+//        }
+//        return s;
+        // 方式二：倒叙遍历按位计算，处理升位问题，最终把结果倒叙一下
+        int temp = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
+            int sum = temp;
+            if (i >= 0) {
+                sum = sum + a.charAt(i) - '0';
+            }
             if (j >= 0) {
-                he = he + Integer.valueOf(String.valueOf(charArrayShort[j]));
+                sum = sum + b.charAt(j) - '0';
             }
-            if (he == 2) {
-                temp = "1";
-                he = 0;
-            } else if (he == 3) {
-                temp = "1";
-                he = 1;
-            } else {
-                temp = "0";
-            }
-            resultArray[i] = (char) (he + '0');
+            stringBuilder.append(sum % 2);
+            temp = sum / 2;
         }
-        String s = new String(resultArray);
-        if (temp.equals("1")) {
-            s = temp + s;
+        if (temp > 0) {
+            stringBuilder.append(temp);
         }
-        return s;
+        return stringBuilder.reverse().toString();
+
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
